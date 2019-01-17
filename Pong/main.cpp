8cfd5717f83b7a692ad2c63170d81fbdf40a83a4
Daniel Korsah <40214314@live.napier.ac.uk>
2019-01-17 11:37:05 +0000

    moving ball, wall bounce

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -18,6 +18,9 @@ const float paddleSpeed = 400.f;
 CircleShape ball;
 RectangleShape paddles[2];
 
+Vector2f ballVelocity;
+bool server = false;
+
 void Load() {
 	// Set size and origin of paddles
 	for (auto &p : paddles) {
@@ -29,9 +32,11 @@ void Load() {
 	ball.setOrigin(ballRadius / 2, ballRadius / 2);
 	// reset paddle position
 	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
-	paddles[1].setPosition(765 + paddleSize.x / 2, gameHeight / 2);
+	paddles[1].setPosition(770 + paddleSize.x / 2, gameHeight / 2);
 	// reset Ball Position
 	ball.setPosition(400, 300);
+	//if server true 100, if server false -100
+	ballVelocity = { (server ? 100.0f : -100.0f), 60.0f };
 }
 
 void Update(RenderWindow &window) {
@@ -53,14 +58,42 @@ void Update(RenderWindow &window) {
 	}
 
 	// handle paddle movement
-	float direction = 0.0f;
+	float direction[2] = {0.0f, 0.0f};
+	//left paddle
 	if (Keyboard::isKeyPressed(controls[0])) {
-		direction--;
+		direction[0]--;
 	}
 	if (Keyboard::isKeyPressed(controls[1])) {
-		direction++;
+		direction[0]++;
 	}
-	paddles[0].move(0, direction * paddleSpeed * dt);
+	paddles[0].move(0, direction[0] * paddleSpeed * dt);
+	//right paddle
+	if (Keyboard::isKeyPressed(controls[2])) {
+		direction[1]--;
+	}
+	if (Keyboard::isKeyPressed(controls[3])) {
+		direction[1]++;
+	}
+	paddles[1].move(0, direction[1] * paddleSpeed * dt);
+
+	// check ball collision
+	const float bx = ball.getPosition().x;
+	const float by = ball.getPosition().y;
+	if (by > gameHeight) { //bottom wall
+	  // bottom wall
+		ballVelocity.x *= 1.1f;
+		ballVelocity.y *= -1.1f;
+		ball.move(0, -10);
+	}
+	else if (by < 0) { //top wall
+   // top wall
+		ballVelocity.x *= 1.1f;
+		ballVelocity.y *= -1.1f;
+		ball.move(0, 10);
+	}
+
+	//translate ball position
+	ball.move(ballVelocity * dt);
 }
 
 void Render(RenderWindow &window) {
