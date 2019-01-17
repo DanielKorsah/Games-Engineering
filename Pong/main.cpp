85da0404ede7aff56c3a51e9c4864a32ee0aa8c4
Daniel Korsah <40214314@live.napier.ac.uk>
2019-01-17 11:15:55 +0000

    swap mains

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -1,21 +1,83 @@
 #include <SFML/Graphics.hpp>
 
-int main() {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML WORKS!");
-	sf::CircleShape shape(100.0f);
-	shape.setFillColor(sf::Color::Green);
+using namespace sf;
+using namespace std;
 
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
+const Keyboard::Key controls[4] = {
+	Keyboard::A,   // Player1 UP
+	Keyboard::Z,   // Player1 Down
+	Keyboard::Up,  // Player2 UP
+	Keyboard::Down // Player2 Down
+};
+const Vector2f paddleSize(25.f, 100.f);
+const float ballRadius = 10.f;
+const int gameWidth = 800;
+const int gameHeight = 600;
+const float paddleSpeed = 400.f;
+
+CircleShape ball;
+RectangleShape paddles[2];
+
+void Load() {
+	// Set size and origin of paddles
+	for (auto &p : paddles) {
+		p.setSize(paddleSize - Vector2f(3, 3));
+		p.setOrigin(paddleSize / 2.f);
+	}
+	// Set size and origin of ball
+	ball.setRadius(ballRadius - 3);
+	ball.setOrigin(ballRadius / 2, ballRadius / 2);
+	// reset paddle position
+	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
+	paddles[1].setPosition(765 + paddleSize.x / 2, gameHeight / 2);
+	// reset Ball Position
+	ball.setPosition(400, 300);
+}
+
+void Update(RenderWindow &window) {
+	// Reset clock, recalculate deltatime
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+	// check and consume events
+	Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			window.close();
+			return;
 		}
-		window.clear();
-		window.draw(shape);
-		window.display();
 	}
 
+	// Quit Via ESC Key
+	if (Keyboard::isKeyPressed(Keyboard::Escape)) {
+		window.close();
+	}
+
+	// handle paddle movement
+	float direction = 0.0f;
+	if (Keyboard::isKeyPressed(controls[0])) {
+		direction--;
+	}
+	if (Keyboard::isKeyPressed(controls[1])) {
+		direction++;
+	}
+	paddles[0].move(0, direction * paddleSpeed * dt);
+}
+
+void Render(RenderWindow &window) {
+	// Draw Everything
+	window.draw(paddles[0]);
+	window.draw(paddles[1]);
+	window.draw(ball);
+}
+
+int main() {
+	RenderWindow window(VideoMode(gameWidth, gameHeight), "PONG");
+	Load();
+	while (window.isOpen()) {
+		window.clear();
+		Update(window);
+		Render(window);
+		window.display();
+	}
 	return 0;
 }
--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,35 +1,21 @@
 #include <SFML/Graphics.hpp>
 
-using namespace sf;
-using namespace std;
+int main() {
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML WORKS!");
+	sf::CircleShape shape(100.0f);
+	shape.setFillColor(sf::Color::Green);
 
-const Keyboard::Key controls[4] = {
-	Keyboard::A,   // Player1 UP
-	Keyboard::Z,   // Player1 Down
-	Keyboard::Up,  // Player2 UP
-	Keyboard::Down // Player2 Down
-};
-const Vector2f paddleSize(25.f, 100.f);
-const float ballRadius = 10.f;
-const int gameWidth = 800;
-const int gameHeight = 600;
-const float paddleSpeed = 400.f;
-
-CircleShape ball;
-RectangleShape paddles[2];
-
-void Load() {
-	// Set size and origin of paddles
-	for (auto &p : paddles) {
-		p.setSize(paddleSize - Vector2f(3, 3));
-		p.setOrigin(paddleSize / 2.f);
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
 	}
-	// Set size and origin of ball
-	ball.setRadius(ballRadius - 3);
-	ball.setOrigin(ballRadius / 2, ballRadius / 2);
-	// reset paddle position
-	paddles[0].setPosition(10 + paddleSize.x / 2, gameHeight / 2);
-	paddles[1].setPosition(765 + paddleSize.x / 2, gameHeight / 2);
-	// reset Ball Position
-	ball.setPosition(400, 300);
+
+	return 0;
 }
