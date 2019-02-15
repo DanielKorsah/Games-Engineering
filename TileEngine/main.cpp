c55989129f99b424f0b9f34ca32765d23ee0317a
Korsah <40214314@live.napier.ac.uk>
2019-02-15 16:34:15 +0000

    set up entity class

--- TileEngine/main.cpp
+++ TileEngine/main.cpp
@@ -1,19 +1,46 @@
 #include <SFML/Graphics.hpp>
+#include <ctime>
+#include <iostream>
+
+using namespace sf;
+
+void Load()
+{
+
+}
+
+void Update(RenderWindow &window)
+{
+
+	// Reset clock, recalculate deltatime
+	static Clock clock;
+	float dt = clock.restart().asSeconds();
+	// check and consume events
+	Event event;
+	while (window.pollEvent(event)) {
+		if (event.type == Event::Closed) {
+			window.close();
+			return;
+		}
+	}
+
+}
+
+void Render(RenderWindow &w)
+{
+
+}
+
 
 int main() {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "TileEngine");
-	sf::CircleShape shape(100.0f);
-	shape.setFillColor(sf::Color::Green);
 
-	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
-		}
+	Load();
+	while (window.isOpen())
+	{
 		window.clear();
-		window.draw(shape);
+		Update(window);
+		Render(window);
 		window.display();
 	}
 
